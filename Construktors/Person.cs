bd7814ee74a634f02b3fda436d3a379121d1c0ee
Pekrol <krzysztof.pekrol@gmail.com>
2019-07-05 23:31:33 +0200

    skonczone konstruktory

---
+++ Construktors/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Construktors
+{
+    class Person
+    {
+        int id;
+        string firstName;
+        string lastName;
+        string address;
+        int age;
+        string gender;
+
+        public Person()
+        {
+            id = 1;
+            firstName = "Marcin";
+            lastName = "Kowal";
+            age = 14;
+        }
+        public Person(int id, string firstName, string lastName, string address, int age, string gender)
+        {
+            this.id = id;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.age = age;
+            this.gender = gender;
+        }
+        public Person(int id, string firstName, string lastName)
+        {
+            this.id = id;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            address = "Katowice";
+            age = 18;
+        }
+        public Person(int id, string firstName)
+        {
+            this.id = id;
+            this.firstName = firstName;
+        }
+    }
+}
